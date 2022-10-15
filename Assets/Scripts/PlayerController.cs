5c3ffd8effe8dbbef28cd8c16f991d3f81563334
LauraTDauden <lauratdauden@gmail.com>
2022-10-15 12:22:57 +0100

    add pickups :zap:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,4 +29,13 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+
+    }
 }
